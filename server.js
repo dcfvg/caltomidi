170ae12811d6111eba89e3e653d9aba552f77d71
dcfvg <qwsx@dcfvg.com>
2015-02-08 17:32:10 +0100

    sort

--- server.js
+++ server.js
@@ -1,46 +1,41 @@
-var fs = require('fs');
-var parser = require('vdata-parser');
-var _ = require('underscore');
-var say = require('say');
-var moment = require('moment');
-var s = require("underscore.string");	
-
-var currentEvent=0;
-var lyrics;
+var fs 			= require('fs');
+var parser 	= require('vdata-parser');
+var _ 			= require('underscore');
+var say 		= require('say');
+var moment 	= require('moment');
+var s 			= require("underscore.string");	
 
 parser.fromFile(__dirname + '/cal.ics', function (err, data) {
 
+	var events = data.VCALENDAR.VEVENT;
+	var currentEvent = 0;
+	var lyrics;
+	
 	function sayNext(){
-		var e = data.VCALENDAR.VEVENT[currentEvent];
+		var e = events[currentEvent];
 
 		var start =	moment(tc(e["DTSTART;TZID=Europe/Paris"]), moment.ISO_8601);
-		var end =	moment(tc(e["DTEND;TZID=Europe/Paris"]), moment.ISO_8601);
-
+		var end 	=	moment(tc(e["DTEND;TZID=Europe/Paris"]), moment.ISO_8601);
 
 		console.log(
-			start.format("DD MM YYYY, HH:mm:ss") + "\t -> \t" + end.format("DD MM YYYY, HH:mm:ss") 
+			"\t" + e.DTSTAMP
+			+ "\t" + tc(e.DTSTAMP) 
+			+ "\t" + start.format("DD MM YYYY, HH:mm:ss") + "\t -> \t" + end.format("DD MM YYYY, HH:mm:ss")
 			+ "\t" + end.from(start, true) 
-			+ "\t \t \t" + e.SUMMARY
-		);
+			+ "\t \t \t" + e.SUMMARY 
+
+		);//	console.log(e);
+
 
 		say.speak('Vicki', e.SUMMARY, sayNext);
 		currentEvent++;
 	}
-	function tc(time){
-		var ts = time;
-		ts = s.insert(ts, 4, '-');
-		ts = s.insert(ts, 7, '-');
-		ts = s.insert(ts, 13, ':');
-		ts = s.insert(ts, 16, ':');
-		return ts
-	}
 
 	if (err) throw err;
 
-	console.log(data.VCALENDAR.VEVENT[37]);
 
-
-	_.each(data.VCALENDAR.VEVENT, function(e, index) {
+	var events = _.sortBy(events, function(e){ return tc(e.DTSTAMP);});
+	_.each(events, function(e, index) {
 
 		var mom = tc(e.DTSTAMP);
 
@@ -58,7 +53,7 @@ parser.fromFile(__dirname + '/cal.ics', function (err, data) {
 	// one by one
 	sayNext();
 
-	console.log(data.VCALENDAR.VEVENT[13]['LAST-MODIFIED']);
+	//console.log(events[13]['LAST-MODIFIED']);
 
 
 	// save a json
@@ -75,3 +70,14 @@ parser.fromFile(__dirname + '/cal.ics', function (err, data) {
 		else console.log("JSON saved to " + lyricsFilename);
 	});  
 });
+
+function tc(time){
+	// reformat ICS TIMECODE
+
+	var ts = time;
+	ts = s.insert(ts, 4, '-');
+	ts = s.insert(ts, 7, '-');
+	ts = s.insert(ts, 13, ':');
+	ts = s.insert(ts, 16, ':');
+	return ts
+}
