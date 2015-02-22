e5416e95d60b190979d5bc599035358d42d6df01
dcfvg <qwsx@dcfvg.com>
2015-02-22 21:01:20 +0100

    rough prototype

--- server.js
+++ server.js
@@ -3,81 +3,72 @@ var parser 	= require('vdata-parser');
 var _ 			= require('underscore');
 var say 		= require('say');
 var moment 	= require('moment');
-var s 			= require("underscore.string");	
+var s 			= require("underscore.string");
 
-parser.fromFile(__dirname + '/cal.ics', function (err, data) {
+function tc(time){ 
+	// reformat ICS TIMECODE
+	time = s.insert(time, 4, '-');
+	time = s.insert(time, 7, '-');
+	time = s.insert(time, 13, ':');
+	time = s.insert(time, 16, ':');
 
-	var events = data.VCALENDAR.VEVENT;
-	var currentEvent = 0;
-	var lyrics;
-	
-	function sayNext(){
-		var e = events[currentEvent];
+	return time;
+}
 
-		var start =	moment(tc(e["DTSTART;TZID=Europe/Paris"]), moment.ISO_8601);
-		var end 	=	moment(tc(e["DTEND;TZID=Europe/Paris"]), moment.ISO_8601);
+function getTiming(e){
 
-		console.log(
-			"\t" + e.DTSTAMP
-			+ "\t" + tc(e.DTSTAMP) 
-			+ "\t" + start.format("DD MM YYYY, HH:mm:ss") + "\t -> \t" + end.format("DD MM YYYY, HH:mm:ss")
-			+ "\t" + end.from(start, true) 
-			+ "\t \t \t" + e.SUMMARY 
+	var start 	=	moment(tc(e["DTSTART;TZID=Europe/Paris"]), moment.ISO_8601),
+			end 		=	moment(tc(e["DTEND;TZID=Europe/Paris"]), moment.ISO_8601),
+			hDuration = end.from(start, true),
+			startX 	=	start.format("X"),
+			endX 		=	end.format("X"),
+			duration= (endX-startX)
+			;
 
-		);//	console.log(e);
+	return {
+		start    	: 	start,
+		end 		 	: 	end,
+		startX    : 	startX,
+		endX 		 	: 	endX,
+		duration 	: 	duration,
+		hDuration :  	hDuration
+	} 
+}
+function addToTimeline(i){
 
+	var e = events[i];
+	var timing = getTiming(e);
+	var t = (timing.startX - t0.startX) / speed;
 
-		say.speak('Vicki', e.SUMMARY, sayNext);
-		currentEvent++;
+	console.log("in ", t/1000, "s : \t\t"+e.SUMMARY);
+
+	if(t != "NaN" && t>0){
+		setTimeout(function(){
+			console.log(timing.start.format("DD MM YYYY, HH:mm:ss"),"\t", timing.hDuration,"\t", e.SUMMARY );
+		}, t);
 	}
+}
 
-	if (err) throw err;
+var events,
+		curEvt = 0,  
+		t0 = 0,
+		speed = 1000;
 
+parser.fromFile(__dirname + '/bv.ics', function (err, data) {
 
-	var events = _.sortBy(events, function(e){ return tc(e.DTSTAMP);});
+	events = _.sortBy(data.VCALENDAR.VEVENT, function(e){ return tc(e.DTSTAMP);});
+	t0 = getTiming(events[28]); // first valid date 
+
 	_.each(events, function(e, index) {
+		addToTimeline(index);
+	});
 
-		var mom = tc(e.DTSTAMP);
-
-		console.log(moment(mom, moment.ISO_8601).format("Do MMMM YYYY, h:mm:ss"));
-		console.log(e.SUMMARY);
-		console.log(" ");
-
-		lyrics = lyrics+' '+ e.SUMMARY;
-	}, this);
-
-
-	// all song
-	//say.speak('Good News', lyrics);
-
-	// one by one
-	sayNext();
-
-	//console.log(events[13]['LAST-MODIFIED']);
-
-
-	// save a json
+	// // save a json
 	var outputFilename = __dirname + '/bv.json';
 	fs.writeFile(outputFilename, JSON.stringify(data, null, 4), function(err) {
 		if(err) console.log(err);
 		else console.log("JSON saved to " + outputFilename);
 	});
 
-	// lyrics
-	var lyricsFilename = __dirname + '/bv.txt';
-	fs.writeFile(lyricsFilename, lyrics, function(err) {
-		if(err) console.log(err);
-		else console.log("JSON saved to " + lyricsFilename);
-	});  
 });
 
-function tc(time){
-	// reformat ICS TIMECODE
-
-	var ts = time;
-	ts = s.insert(ts, 4, '-');
-	ts = s.insert(ts, 7, '-');
-	ts = s.insert(ts, 13, ':');
-	ts = s.insert(ts, 16, ':');
-	return ts
-}
